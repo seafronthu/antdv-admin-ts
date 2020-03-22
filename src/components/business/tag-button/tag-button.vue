b39d9dd828372fa86ebbc714f9de65f9ca8d579b
hhf <1170533231@qq.com>
2020-03-22 23:13:09 +0800

    update

--- src/components/business/tag-button/tag-button.vue
+++ src/components/business/tag-button/tag-button.vue
@@ -1,5 +1,32 @@
 <!-- 标签按钮 -->
 <template>
+  <div class="tag-button" :style="{ height: height }">
+    <el-dropdown :trigger="trigger" @command="handleCommand">
+      <div ref="clickEle">
+        <div @click.stop="handleClick" ref="contextmenuEle">
+          <el-tag
+            :type="type"
+            :color="color"
+            :effect="effect"
+            :closable="closable"
+            class="tag-button-flex"
+          >
+            <IconFont
+              v-if="dot"
+              icon="dot"
+              :style="{ color: color, paddingRight: '10px' }"
+            />
+            <slot></slot>
+          </el-tag>
+        </div>
+      </div>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <slot name="menu"></slot>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
   <div :style="{ height: height }">
     <a-dropdown :trigger="trigger">
       <a-tag
@@ -24,8 +51,44 @@
   </div>
 </template>
 
-<script>
-import IconFont from "@hhf/icon-font";
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import IconFont from "@h/icon-font";
+@Component({
+  components: {
+    IconFont
+  }
+})
+export default class TagButton extends Vue {
+  @Prop({
+    type: Array,
+    default() {
+      return ["hover"];
+    }
+  })
+  trigger?: ("hover" | "click" | "contextmenu")[];
+  @Prop({
+    type: Boolean
+  })
+  closable?: boolean;
+  @Prop({
+    type: Boolean,
+    default: false
+  })
+  dot?: boolean;
+  @Prop({ type: String }) color?: string;
+  @Prop({ type: String, default: "" })
+  type?: "" | "success" | "info" | "warning" | "danger";
+  @Prop({
+    type: String
+  })
+  height?: string;
+  handleClose() {}
+  handleTagClick() {
+    this.$emit("trigger-click");
+  }
+  handleMenuClick() {}
+}
 export default {
   name: "TagButton",
 
